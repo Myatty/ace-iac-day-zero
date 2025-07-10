c71947593f3547f507a1dabf5269ef42a8a0f963
Myint Myat Pyae Sone <myintmyatps664@gmail.com>
2025-07-11 00:33:28 +0630

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "DevOps_Myat"
     workspaces {
       name = "ace-iac-day-zero"
     }
